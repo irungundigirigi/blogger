a4ed06af3c9ee21c88e191eae8b71086ff75a732
Edwin Irungu <irungundigirigi@gmail.com>
2021-08-29 10:07:57 +0300

    server connection sucessfull

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -3,14 +3,36 @@ import './Homepage.css';
 import Logo from '../../assets/Logo.png';
 import Login from './Login/Login';
 import Signup from './Sign up/Signup';
+import axios from 'axios';
 
 
 function Homepage() {
+    //states
     const[login, setLogin] = useState({login:true,signup:false})
+    const[form, setForm] = useState({});
+    
+    //function to toggle signup/
     const toggleLogin = (e) => {
         e.preventDefault()
         setLogin({login:false,signup:true})
     }
+    //function to handle signup
+    const handleSignup =  async (e) => {
+        e.preventDefault()
+        let formDataRecords = {"name":form.name, "email":form.email, "password":form.password} ;
+        await axios.post("http://localhost:3002/user",formDataRecords).then(response => console.log(response.data))
+        console.log(formDataRecords)
+        
+    }
+
+    const handleInputChange = async (e) => {
+        const value = e.target.value;
+        await setForm({...form,
+        [e.target.name]:value
+        });
+       
+    }
+    
     return (
         <div className="Main-container">
             <div className="Homepage">
@@ -22,7 +44,7 @@ function Homepage() {
                 </div>
                 <div className="Sign_Login">
                     {login.login && <Login onChildClick={toggleLogin}/>}
-                    {login.signup && <Signup />}
+                    {login.signup && <Signup onInputChange={handleInputChange}  onSignupClick ={handleSignup} formData={form}/>}
                 </div>
             </div>
         </div>
--- src/pages/Homepage/Sign up/Signup.js
+++ src/pages/Homepage/Sign up/Signup.js
@@ -1,26 +1,28 @@
 import React from 'react'
+import './Signup.css';
 
-function Signup() {
+function Signup(props) {
     return (
         <div>
         <h2>SIGN UP</h2>
         <form className='login-form'>
-            <div className="input-holder">
-                    <label>Email</label>
-                    <input type="text" placeholder="Enter email" className="Email" required />
-            </div>
-            <div className="input-holder">
+        <div className="input-holder">
                 <label><i class="fa fa-user-o" aria-hidden="true"></i>
-                    Username</label>
-                <input type="text" placeholder="Enter username" className="Username" required>
+                    Name</label>
+                <input  value={props.formData.name} onChange={props.onInputChange} type="text" placeholder="Enter name" className="Username" name="name"  required>
                 </input>
             </div>
             <div className="input-holder">
+                    <label>Email</label>
+                    <input value={props.formData.email} onChange={props.onInputChange} type="text" placeholder="Enter email" className="Email" name="email" required />
+            </div>
+           
+            <div className="input-holder">
                 <label>Password</label>
-                <input type="password" placeholder="Enter password" className="Password" required />
+                <input value={props.formData.password}  onChange={props.onInputChange} type="password" placeholder="Enter password" className="Password" name="password" required />
             </div>
             <div className="input-holder">
-                <button type="submit">Login</button>
+                <button onClick= {props.onSignupClick} type="submit">Signup</button>
             </div>
         </form>
         
