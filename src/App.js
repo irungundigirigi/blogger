9e218df7da1d51959c1e39ec8889b4bc7668c3c8
Edwin Irungu <irungundigirigi@gmail.com>
2021-09-21 13:41:25 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, {Component} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
+import Feed from "./pages/Feed/Feed";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Homepage} />
+          <Route path="/feed" component={Feed} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Homepage/Login/Login.js
+++ src/pages/Homepage/Login/Login.js
@@ -7,19 +7,20 @@ function Login(props) {
     return (
         <div>
             <h2>LOGIN</h2>
+            <p className="error-holder">{props.error.error}</p>
             <form className='login-form'>
                 <div className="input-holder">
                     <label><i class="fa fa-user-o" aria-hidden="true"></i>
-                        Username</label>
-                    <input type="text" placeholder="Enter username" className="Username" required>
+                        Email</label>
+                    <input type="text" value={props.loginData.email}   placeholder="Enter email" className="Email" name="email" onChange={props.onLoginInputChange} required>
                     </input>
                 </div>
                 <div className="input-holder">
                     <label>Password</label>
-                    <input type="password" placeholder="Enter password" className="Password" required />
+                    <input type="password" value={props.loginData.password}  placeholder="Enter password" className="Password" name="password" onChange={props.onLoginInputChange} required />
                 </div>
                 <div className="input-holder">
-                    <button type="submit">Login</button>
+                    <button type="submit" onClick = {props.onLoginClick}>Login</button>
                 </div>
                 <div className="input-holder">
                     <p> Dont have an account? <button onClick={props.onChildClick}>Sign up</button> </p>
